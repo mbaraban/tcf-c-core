4191bbcef38ee288c918b92a9d40450a4655b5a7
Didier <didier.brachet@windriver.com>
2016-01-22 10:27:24 +0100

    Fixed copyright

--- wr_extensions/agent/tcf/framework/channel_np.c
+++ wr_extensions/agent/tcf/framework/channel_np.c
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2014 Wind River Systems, Inc. and others.
+ * Copyright (c) 2015, 2016 Wind River Systems, Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * and Eclipse Distribution License v1.0 which accompany this distribution.
@@ -11,8 +11,6 @@
  *
  * Contributors:
  *     Wind River Systems - initial API and implementation
- *     Michael Sills-Lavoie(École Polytechnique de Montréal)  - ZeroCopy support
- *              *                         *            - tcp_splice_block_stream
  *******************************************************************************/
 
 /*
